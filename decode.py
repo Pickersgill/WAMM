62808f5054a4329ba1d1e4af193e287b43fd20f7
Pickersgill <Jack.Pellen-Pickersgill.2018@live.rhul.ac.uk>
2022-07-20 16:42:15 +0100

    removed media

--- decode.py
+++ decode.py
@@ -1,6 +1,8 @@
 import sys
 import re
 
+MEDIA = "<Media omitted>"
+
 def decode(chat):
     expr = r"\d\d/\d\d/\d\d\d\d, \d\d:\d\d - (.*?): (.*)$"
     msgs = []
@@ -9,7 +11,7 @@ def decode(chat):
         l = re.sub(r"[\r\n]", "", l)
         m = re.search(expr, l)
 
-        if m:
+        if m and m.group(2) != MEDIA:
             msgs.append(m.group(2))
 
     return msgs
--- text_token/tokenizer.py
+++ text_token/tokenizer.py
@@ -1,22 +1,8 @@
 import re
 import pandas as pd
 
-def tokenize(card):
-    name = card["name"]
-    shortname = name.split(",")[0]
-    text = card["text"]
-    retext = re.sub(r"[\(.*?\)]", "", text)
-    retext = "{START}" + re.sub("%s|%s" % (name, shortname), "{THIS}", retext) + "{END}"
-
-    matches = re.findall(r"([0-9a-zA-Z'-+]+)|(\{[^}]+\})|([,:./])", retext)
-    tokens = []
-    for m in matches:
-        tokens.append("".join(list(m)))
-    
-    return tokens
-    
-def tokenize_string(obj):
-    tokens = ["{START}"] + obj.split(" ") + ["{END}"]
+def tokenize_string(text):
+    tokens = ["{START}"] + text.split(" ") + ["{END}"]
     return tokens
     
 
